4755eb4e70cc76e5c250b93f890d116584acae53
bwrosen20 <bwrosen20@gmail.com>
2023-04-02 13:49:29 -0400

    solved form bug

--- jeopardy/src/components/NewQuestionForm.js
+++ jeopardy/src/components/NewQuestionForm.js
@@ -2,20 +2,26 @@ import React,{useState,useEffect} from "react"
 
 function NewQuestionForm(){
 
-
-  const [timeRemaining,setTimeRemaining]=useState(3)
   const [submitted,setSubmitted]=useState(false)
-  const [formData,setFormData]=useState({
+  let [formData,setFormData]=useState({
     prompt:"",
     correctAnswers:[],
-    className:""
+    className:"",
+    value:0
   })
 
   function handleChange(event){
+   
     setFormData({
       ...formData,
-      [event.target.name]:event.target.value
+      [event.target.name]:event.target.value,
+      ["value"]:((event.target.value).includes("200"))?200:((event.target.value).includes("400"))?400:((event.target.value).includes("600"))?600:((event.target.value).includes("800"))?800:1000
+      
+      
     })
+
+
+
   }
 
   function afterSubmit(){
@@ -27,10 +33,9 @@ function NewQuestionForm(){
   }
 
 
-  function handleSubmit(event){
+  function handleSubmit(){
     setSubmitted(true)
     afterSubmit();
-    event.preventDefault();
     fetch("http://localhost:3001/questions",{
       method:"POST",
       headers:{
@@ -49,6 +54,7 @@ function NewQuestionForm(){
         <label>
           Prompt:
           <input
+            placeholder="Please input question"
             className="forminput"
             type="text"
             name="prompt"
@@ -59,6 +65,7 @@ function NewQuestionForm(){
         <label>
           correct answer:
           <input
+            placeholder="Please input answer"
             type="text"
             name="correctAnswers"
             value={formData.correctAnswers}
@@ -68,10 +75,12 @@ function NewQuestionForm(){
         <label>
           Category/value:
           <select
+            placeholder="Select category/value"
             name="className"
             value={formData.className}
             onChange={handleChange}
           >
+            <option value="none">Select category/value</option>
             <option value="movies200">movies200</option>
             <option value="movies400">movies400</option>
             <option value="movies600">movies600</option>
@@ -82,11 +91,11 @@ function NewQuestionForm(){
             <option value="tv600">tv600</option>
             <option value="tv800">tv800</option>
             <option value="tv1000">tv1000</option>
-            <option value="geography200">geography/science200</option>
-            <option value="geography400">geography/science400</option>
-            <option value="geography600">geography/science600</option>
-            <option value="geography800">geography/science800</option>
-            <option value="geography1000">geography/science1000</option>
+            <option value="geography200">misc200</option>
+            <option value="geography400">misc400</option>
+            <option value="geography600">misc600</option>
+            <option value="geography800">misc800</option>
+            <option value="geography1000">misc1000</option>
             <option value="sports200">sports200</option>
             <option value="sports400">sports400</option>
             <option value="sports600">sports600</option>
--- jeopardy/src/components/Rules.js
+++ jeopardy/src/components/Rules.js
@@ -5,7 +5,7 @@ function Rules(){
     return <div>
             <h1 className="center">Jeopardy Rules</h1>
             <h3>1. Two teams will compete in the game</h3>
-            <h3>2. Each team will take a turn beginning with team 1</h3>
+            <h3>2. Each team will take a turn beginning with team 1, team 2 and repeating</h3>
             <h3>3. You will have 15 seconds to answer the question. 
                 If you cannot, the other team will have 10 seconds to steal</h3>
             <h3>4. Points are awarded to whichevr team -if any- gets the question right</h3>
